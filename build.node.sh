d5a9cba8b95f65a668e426d95f474593a660a11a
dapplion <guillem.lion.benosa@gmail.com>
2020-10-08 09:28:23 +0200

    Add build script

---
+++ build.node.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+BLST_WRAP_PREBUILD=blst_wrap.cpp
+BLST_WRAP_OUTPUT=blst/bindings/node.js
+BLST_NODE_OUTPUT=blst/bindings/node.js/blst.node
+BLST_NODE_TARGET=build/Release/blst.node
+
+cp $BLST_WRAP_PREBUILD $BLST_WRAP_OUTPUT
+
+(cd blst/bindings/node.js; ./run.me)
+
+mkdir -p `dirname $BLST_NODE_TARGET`
+cp $BLST_NODE_OUTPUT $BLST_NODE_TARGET
