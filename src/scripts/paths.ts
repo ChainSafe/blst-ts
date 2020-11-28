9322f013730c21f36d9c8cc0c89d4e79bdccd5f4
dapplion <guillem.lion.benosa@gmail.com>
2020-11-28 10:29:26 +0000

    Find variable target blst.node path

--- src/scripts/paths.ts
+++ src/scripts/paths.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 
 const rootDir = path.join(__dirname, "../..");
+const bindingsName = "blst.node";
 
 export const packageJsonPath = path.join(rootDir, "package.json");
 
@@ -9,7 +10,6 @@ export const bindingsDirSrc = path.join(rootDir, "blst/bindings/node.js");
 export const blstWrapCppName = "blst_wrap.cpp";
 export const prebuiltSwigSrc = path.join(rootDir, "prebuild", blstWrapCppName);
 export const prebuiltSwigTarget = path.join(bindingsDirSrc, blstWrapCppName);
-export const bindingsSrc = path.join(bindingsDirSrc, "blst.node");
 
 export const defaultBinaryDir = path.join(rootDir, "prebuild");
 
@@ -45,6 +45,47 @@ export function ensureDirFromFilepath(filepath: string) {
   if (!fs.existsSync(dirpath)) {
     fs.mkdirSync(dirpath, { recursive: true });
   }
+}
+
+/**
+ * The output of node-gyp is not at a predictable path but various
+ * depending on the OS.
+ * Paths based on https://github.com/TooTallNate/node-bindings/blob/c8033dcfc04c34397384e23f7399a30e6c13830d/bindings.js#L36
+ */
+export function findBindingsFile(dirpath: string): string {
+  const filepaths = [
+    // In dirpath
+    [dirpath, bindingsName],
+    // node-gyp's linked version in the "build" dir
+    [dirpath, "build", bindingsName],
+    // node-waf and gyp_addon (a.k.a node-gyp)
+    [dirpath, "build", "Debug", bindingsName],
+    [dirpath, "build", "Release", bindingsName],
+    // Debug files, for development (legacy behavior, remove for node v0.9)
+    [dirpath, "out", "Debug", bindingsName],
+    [dirpath, "Debug", bindingsName],
+    // Release files, but manually compiled (legacy behavior, remove for node v0.9)
+    [dirpath, "out", "Release", bindingsName],
+    [dirpath, "Release", bindingsName],
+    // Legacy from node-waf, node <= 0.4.x
+    [dirpath, "build", "default", bindingsName],
+    // Production "Release" buildtype binary (meh...)
+    [dirpath, "compiled", "version", "platform", "arch", bindingsName],
+    // node-qbs builds
+    [dirpath, "addon-build", "release", "install-root", bindingsName],
+    [dirpath, "addon-build", "debug", "install-root", bindingsName],
+    [dirpath, "addon-build", "default", "install-root", bindingsName],
+    // node-pre-gyp path ./lib/binding/{node_abi}-{platform}-{arch}
+    [dirpath, "lib", "binding", "nodePreGyp", bindingsName],
+  ].map((pathParts) => path.join(...pathParts));
+
+  for (const filepath of filepaths) {
+    if (fs.existsSync(filepath)) {
+      return filepath;
+    }
+  }
+
+  throw Error(`Could not find bindings file. Tried:\n${filepaths.join("\n")}`);
 }
 
 export class NotNodeJsError extends Error {
