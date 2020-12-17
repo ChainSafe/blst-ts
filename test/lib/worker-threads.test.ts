96198d9994373c2a9cf5568b6e504af051633f38
dapplion <guillem.lion.benosa@gmail.com>
2020-12-17 16:58:57 +0100

    Defer importing worker_threads

--- test/lib/worker-threads.test.ts
+++ test/lib/worker-threads.test.ts
@@ -1,16 +1,16 @@
-import { Worker } from "worker_threads";
-
 describe("worker_threads test", function () {
   const nodeJsSemver = process.versions.node;
   const nodeJsMajorVer = parseInt(nodeJsSemver.split(".")[0]);
 
-  it("Should not throw when importing in two threads", function () {
+  it("Should not throw when importing in two threads", async function () {
     if (!nodeJsMajorVer) {
       throw Error(`Error parsing NodeJS version: ${nodeJsSemver}`);
     }
     if (nodeJsMajorVer < 12) {
       this.skip();
     }
+
+    const { Worker } = await import("worker_threads");
 
     // Create two workers so blst.node is imported twice to trigger the error
     // blst/bindings/node.js$ node worker-threads.js
