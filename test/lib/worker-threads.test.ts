f3e0c11e2b9ad09f6ef03ad11a1c38db610b6c01
dapplion <guillem.lion.benosa@gmail.com>
2020-12-17 16:53:10 +0100

    Skip worker_threads test if NodeJS ver < 12

--- test/lib/worker-threads.test.ts
+++ test/lib/worker-threads.test.ts
@@ -1,7 +1,17 @@
 import { Worker } from "worker_threads";
 
-describe("worker_threads test", () => {
-  it("Should not throw when importing in two threads", () => {
+describe("worker_threads test", function () {
+  const nodeJsSemver = process.versions.node;
+  const nodeJsMajorVer = parseInt(nodeJsSemver.split(".")[0]);
+
+  it("Should not throw when importing in two threads", function () {
+    if (!nodeJsMajorVer) {
+      throw Error(`Error parsing NodeJS version: ${nodeJsSemver}`);
+    }
+    if (nodeJsMajorVer < 12) {
+      this.skip();
+    }
+
     // Create two workers so blst.node is imported twice to trigger the error
     // blst/bindings/node.js$ node worker-threads.js
     // Error: Module did not self-register: 'blst/bindings/node.js/blst.node'.
