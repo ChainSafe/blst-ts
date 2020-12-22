291032eea649f945be4d05332ae4c874c5ca4b17
dapplion <guillem.lion.benosa@gmail.com>
2020-12-22 09:17:21 +0100

    Update blst_wrap.py script

--- blst_wrap.py
+++ blst_wrap.py
@@ -3,60 +3,54 @@ import subprocess
 import re
 import shutil
 import os
-import os.path
 
-
-pythonScript = sys.argv[0]
-# ../blst.swg
-SOURCE_SWIG_FILE = sys.argv[1]
-# <(INTERMEDIATE_DIR)/blst_wrap.cpp
-# In Github actions: /home/runner/work/blst-ts/blst-ts/blst/bindings/node.js/build/Release/obj.target/blst/geni/blst_wrap.cpp
-BLST_WRAP_CPP_TARGET = sys.argv[2]
+BLST_WRAP_CPP = sys.argv[2]     # see binding.gyp
 BLST_WRAP_CPP_PREBUILD = os.getenv('BLST_WRAP_CPP_PREBUILD')
-SWIG_SKIP_RUN = os.getenv('SWIG_SKIP_RUN')
 
-print("SOURCE_SWIG_FILE", SOURCE_SWIG_FILE)
-print("BLST_WRAP_CPP_TARGET", BLST_WRAP_CPP_TARGET)
-print("BLST_WRAP_CPP_PREBUILD", BLST_WRAP_CPP_PREBUILD)
-print("SWIG_SKIP_RUN", SWIG_SKIP_RUN)
-print("CWD", os.getcwd())
-
-
-if BLST_WRAP_CPP_PREBUILD and os.path.isfile(BLST_WRAP_CPP_PREBUILD):
-    print("Copying and using BLST_WRAP_CPP_PREBUILD")
-    shutil.copyfile(BLST_WRAP_CPP_PREBUILD, BLST_WRAP_CPP_TARGET)
-    sys.exit(0)
-else:
-    if SWIG_SKIP_RUN:
-        print("BLST_WRAP_CPP_PREBUILD not found, but it should exist since SWIG_SKIP_RUN=true")
-        sys.exit(201)
+if BLST_WRAP_CPP_PREBUILD:
+    if os.path.isfile(BLST_WRAP_CPP_PREBUILD):
+        print("Copying and using '{}'".format(BLST_WRAP_CPP_PREBUILD))
+        shutil.copyfile(BLST_WRAP_CPP_PREBUILD, BLST_WRAP_CPP)
+        sys.exit(0)
     else:
-        print("BLST_WRAP_CPP_TARGET not found, building")
+        print("'{}' not found, attempt to swig".format(BLST_WRAP_CPP_PREBUILD))
 
 try:
     version = subprocess.check_output(["swig", "-version"]).decode('ascii')
-    print(version)
-    v = re.search(r'SWIG Version ([0-9]+)', version)
-    if v and int(v.group(1)) >= 4:
-        print("Running SWIG...")
+    v = re.search(r'(SWIG Version ([0-9]+).*)', version)
+    if not v:
+        raise OSError(2, "unparseable output from 'swig -version'")
+    if int(v.group(2)) >= 4:
         subprocess.check_call(["swig", "-c++", "-javascript",
                                        "-node", "-DV8_VERSION=0x060000",
-                                       "-o", BLST_WRAP_CPP_TARGET, SOURCE_SWIG_FILE])
+                                       "-o", BLST_WRAP_CPP, sys.argv[1]])
+        if BLST_WRAP_CPP_PREBUILD:
+            print("Copying blst_wrap.cpp to '{}'".format(BLST_WRAP_CPP_PREBUILD))
+            shutil.copyfile(BLST_WRAP_CPP, BLST_WRAP_CPP_PREBUILD)
     else:
-        print("Unsupported swig version")
-        sys.exit(202)
-    
+        raise OSError(201, "unsupported " + v.group(1))
+    sys.exit(0)
 except OSError as e:
-    if e.errno == 2:    # "no such file or directory"
-        print("SWIG not installed", e)
-    else:
-        print("Error checking SWIG version", e)
-    sys.exit(e.errno)
+    if e.errno != 2:    # not "no such file or directory"
+        raise e
+    sys.stderr.write("{}\n".format(e))
+    sys.exit(e.errno)   # or do something else, say ...
 
+here = os.path.split(sys.argv[0])
 
-if BLST_WRAP_CPP_PREBUILD:
-    print("Copying built BLST_WRAP_CPP_TARGET to BLST_WRAP_CPP_PREBUILD")
-    shutil.copyfile(BLST_WRAP_CPP_TARGET, BLST_WRAP_CPP_PREBUILD)
+version = subprocess.check_output(["node", "--version"]).decode('ascii')
+v = re.match(r'^v([0-9]+)', version)
+if v:
+    maj = int(v.group(1))
+    if maj >= 16:
+        pass
+    elif maj >= 12:
+        pre_gen = "blst_wrap.v12.cpp"
+    elif maj >= 8:
+        pre_gen = "blst_wrap.v8.cpp"
 
-
-print("Done")
+try:
+    shutil.copyfile(os.path.join(here[0], pre_gen), BLST_WRAP_CPP)
+except NameError:
+    sys.stderr.write("unsupported 'node --version': {}".format(version))
+    sys.exit(202)         # "no such file or directory"
