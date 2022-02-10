ce9af8fa14b2f7825d697ecb2d01295a2644031b
dapplion <35266934+dapplion@users.noreply.github.com>
2022-02-10 10:05:01 +0530

    Download only required tests

--- test/spec/downloadTests.ts
+++ test/spec/downloadTests.ts
@@ -1,10 +1,11 @@
 import {downloadTests} from "@chainsafe/lodestar-spec-test-util";
-import {SPEC_TEST_LOCATION, SPEC_TEST_VERSION, SPEC_TEST_REPO_URL} from "./specTestVersioning";
+import {SPEC_TEST_LOCATION, SPEC_TEST_VERSION, SPEC_TEST_REPO_URL, SPEC_TEST_TO_DOWNLOAD} from "./specTestVersioning";
 
 /* eslint-disable no-console */
 
 downloadTests(
   {
+    testsToDownload: SPEC_TEST_TO_DOWNLOAD,
     specVersion: SPEC_TEST_VERSION,
     outputDir: SPEC_TEST_LOCATION,
     specTestsRepoUrl: SPEC_TEST_REPO_URL,
--- test/spec/specTestVersioning.ts
+++ test/spec/specTestVersioning.ts
@@ -1,4 +1,4 @@
-import path from "node:path";
+import path from "path";
 
 // WARNING! Don't move or rename this file !!!
 //
