248bc68b022489e3af3e28710f4cbfb163ab5150
dapplion <35266934+dapplion@users.noreply.github.com>
2022-02-10 11:02:25 +0530

    Roll your own runner

--- test/spec/downloadTests.ts
+++ test/spec/downloadTests.ts
@@ -1,17 +1,41 @@
-import {downloadTests} from "@chainsafe/lodestar-spec-test-util";
+import fs from "fs";
+import path from "path";
+import {execSync} from "child_process";
 import {SPEC_TEST_LOCATION, SPEC_TEST_VERSION, SPEC_TEST_REPO_URL, SPEC_TEST_TO_DOWNLOAD} from "./specTestVersioning";
 
 /* eslint-disable no-console */
 
-downloadTests(
-  {
-    testsToDownload: SPEC_TEST_TO_DOWNLOAD,
-    specVersion: SPEC_TEST_VERSION,
-    outputDir: SPEC_TEST_LOCATION,
-    specTestsRepoUrl: SPEC_TEST_REPO_URL,
-  },
-  console.log
-).catch((e: Error) => {
-  console.error(e);
-  process.exit(1);
-});
+const specVersion = SPEC_TEST_VERSION;
+const outputDir = SPEC_TEST_LOCATION;
+const specTestsRepoUrl = SPEC_TEST_REPO_URL;
+
+const versionFile = path.join(outputDir, "version.txt");
+const existingVersion = fs.existsSync(versionFile) && fs.readFileSync(versionFile, "utf8").trim();
+
+if (existingVersion === specVersion) {
+  console.log(`version ${specVersion} already downloaded`);
+  process.exit(0);
+} else {
+  console.log(`Downloading new version ${specVersion}`);
+}
+
+if (fs.existsSync(outputDir)) {
+  console.log(`Cleaning existing version ${existingVersion} at ${outputDir}`);
+  shell(`rm -rf ${outputDir}`);
+}
+
+fs.mkdirSync(outputDir, {recursive: true});
+
+for (const test of SPEC_TEST_TO_DOWNLOAD) {
+  const url = `${specTestsRepoUrl}/releases/download/${specVersion}/${test}.tar.gz`;
+  console.log(`Downloading ${url} to ${outputDir}`);
+  shell(`wget -c ${url} -O - | tar -xz --directory ${outputDir}`);
+  console.log(`Downloaded ${url} to ${outputDir}`);
+}
+
+// If successful
+fs.writeFileSync(versionFile, specVersion);
+
+function shell(cmd: string): string {
+  return execSync(cmd, {encoding: "utf8"}).trim();
+}
