2e6aeebfaec37d1afc375a6ae04449e80e5bc1ad
Bryan White <bryanchriswhite@gmail.com>
2022-06-17 14:22:25 +0200

    fix: make BINDINGS_DIR platform agnostic

--- src/scripts/paths.ts
+++ src/scripts/paths.ts
@@ -3,7 +3,7 @@ import path from "path";
 export const ROOT_DIR = path.join(__dirname, "../..");
 export const PREBUILD_DIR = path.join(ROOT_DIR, "prebuild", "swig");
 export const PACKAGE_JSON_PATH = path.join(ROOT_DIR, "package.json");
-export const BINDINGS_DIR = path.join(ROOT_DIR, "blst/bindings/node.js");
+export const BINDINGS_DIR = path.join(ROOT_DIR, "blst", "bindings", "node.js");
 
 // Paths for blst_wrap.cpp
 // Resolve path to absolute since it will be used from a different working dir
