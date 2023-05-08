c164346ca0e9494f4c0be9e6e1bd1612c16217fe
matthewkeil <me@matthewkeil.com>
2023-05-08 02:13:08 -0400

    refactor(functions): remove unused declarations

--- rebuild/src/functions.h
+++ rebuild/src/functions.h
@@ -16,16 +16,6 @@ namespace Functions
     Napi::Value AggregatePublicKeysSync(const Napi::CallbackInfo &info);
     Napi::Value AggregateSignatures(const Napi::CallbackInfo &info);
     Napi::Value AggregateSignaturesSync(const Napi::CallbackInfo &info);
-    Napi::Value Verify(const Napi::CallbackInfo &info);
-    Napi::Value VerifySync(const Napi::CallbackInfo &info);
-    Napi::Value AggregateVerify(const Napi::CallbackInfo &info);
-    Napi::Value AggregateVerifySync(const Napi::CallbackInfo &info);
-    Napi::Value FastAggregateVerify(const Napi::CallbackInfo &info);
-    Napi::Value FastAggregateVerifySync(const Napi::CallbackInfo &info);
-    Napi::Value VerifyMultipleAggregateSignatures(const Napi::CallbackInfo &info);
-    Napi::Value VerifyMultipleAggregateSignaturesSync(const Napi::CallbackInfo &info);
-    Napi::Value TestSync(const Napi::CallbackInfo &info);
-    Napi::Value TestAsync(const Napi::CallbackInfo &info);
 
     void Init(const Napi::Env &env, Napi::Object &exports);
 }
