29d202f40108793c59646b286effe7bca0a3571a
matthewkeil <me@matthewkeil.com>
2023-06-05 22:33:55 -0400

    docs: add spacing and comments to secret_key similar to public_key

--- rebuild/src/secret_key.cc
+++ rebuild/src/secret_key.cc
@@ -10,6 +10,7 @@ void SecretKey::Init(Napi::Env env, Napi::Object &exports, BlstTsAddon *module)
         InstanceMethod("toPublicKey", &SecretKey::ToPublicKey, static_cast<napi_property_attributes>(napi_enumerable)),
         InstanceMethod("sign", &SecretKey::Sign, static_cast<napi_property_attributes>(napi_enumerable)),
     };
+
     Napi::Function ctr = DefineClass(env, "SecretKey", proto, module);
     module->_secret_key_ctr = Napi::Persistent(ctr);
     module->_secret_key_tag = {BLST_TS_SECRET_KEY_LOWER_TAG, BLST_TS_SECRET_KEY_UPPER_TAG};
@@ -20,6 +21,8 @@ Napi::Value SecretKey::FromKeygen(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
     Napi::EscapableHandleScope scope(env);
+
+    // Pull Value from info and check type is valid
     Napi::Value ikm_value = info[0];
     if (!ikm_value.IsTypedArray())
     {
@@ -32,6 +35,7 @@ Napi::Value SecretKey::FromKeygen(const Napi::CallbackInfo &info)
         Napi::TypeError::New(env, "ikm must be a BlstBuffer").ThrowAsJavaScriptException();
         return env.Undefined();
     }
+    // Convert to final Uint8Array type and check length is valid
     Napi::Uint8Array ikm = ikm_array.As<Napi::TypedArrayOf<uint8_t>>();
     if (ikm.ByteLength() < BLST_TS_SECRET_KEY_LENGTH)
     {
@@ -40,10 +44,17 @@ Napi::Value SecretKey::FromKeygen(const Napi::CallbackInfo &info)
         Napi::TypeError::New(env, msg.str()).ThrowAsJavaScriptException();
         return env.Undefined();
     }
+
+    // Get module for globals and run SecretKey constructor
     BlstTsAddon *module = env.GetInstanceData<BlstTsAddon>();
+    // Pass void External to constructor so can tell if constructor was called
+    // from C or JS to prevent direct calls from JS to ensure proper setup
     Napi::Object wrapped = module->_secret_key_ctr.New({Napi::External<void *>::New(env, nullptr)});
+    // Setup object correctly.  Start with type tagging wrapper class.
     wrapped.TypeTag(&module->_secret_key_tag);
     SecretKey *sk = SecretKey::Unwrap(wrapped);
+    // If `info` string is passed use it otherwise use default without. Is optional
+    // parameter from blst library.
     if (info[1].IsString())
     {
         sk->_key->keygen(ikm.Data(), BLST_TS_SECRET_KEY_LENGTH, info[1].As<Napi::String>().Utf8Value());
@@ -52,12 +63,14 @@ Napi::Value SecretKey::FromKeygen(const Napi::CallbackInfo &info)
     {
         sk->_key->keygen(ikm.Data(), BLST_TS_SECRET_KEY_LENGTH);
     }
+    // Check if key is zero and set flag if so. Several specs depend on this check
     blst::byte key_bytes[BLST_TS_SECRET_KEY_LENGTH];
     sk->_key->to_bendian(key_bytes);
     if (is_zero_bytes(key_bytes, 0, BLST_TS_SECRET_KEY_LENGTH))
     {
         sk->_is_zero_key = true;
     }
+
     return scope.Escape(wrapped);
 }
 
@@ -65,6 +78,8 @@ Napi::Value SecretKey::Deserialize(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
     Napi::EscapableHandleScope scope(env);
+
+    // Pull Value from info and check type is valid
     Napi::Value sk_bytes_value = info[0];
     if (!sk_bytes_value.IsTypedArray())
     {
@@ -77,6 +92,7 @@ Napi::Value SecretKey::Deserialize(const Napi::CallbackInfo &info)
         Napi::TypeError::New(env, "skBytes must be a BlstBuffer").ThrowAsJavaScriptException();
         return env.Undefined();
     }
+    // Convert to final Uint8Array type and check length is valid
     Napi::Uint8Array sk_bytes = sk_bytes_array.As<Napi::TypedArrayOf<uint8_t>>();
     if (sk_bytes.ByteLength() != BLST_TS_SECRET_KEY_LENGTH)
     {
@@ -85,17 +101,24 @@ Napi::Value SecretKey::Deserialize(const Napi::CallbackInfo &info)
         Napi::TypeError::New(env, msg.str()).ThrowAsJavaScriptException();
         return env.Undefined();
     }
+    // Get module for globals and run SecretKey constructor
     BlstTsAddon *module = env.GetInstanceData<BlstTsAddon>();
+    // Pass void External to constructor so can tell if constructor was called
+    // from C or JS to prevent direct calls from JS to ensure proper setup
     Napi::Object wrapped = module->_secret_key_ctr.New({Napi::External<void *>::New(env, nullptr)});
+    // Setup object correctly.  Start with type tagging wrapper class.
     wrapped.TypeTag(&module->_secret_key_tag);
     SecretKey *sk = SecretKey::Unwrap(wrapped);
+    // Deserialize key
     sk->_key->from_bendian(sk_bytes.Data());
+    // Check if key is zero and set flag if so. Several specs depend on this check
     blst::byte key_bytes[BLST_TS_SECRET_KEY_LENGTH];
     sk->_key->to_bendian(key_bytes);
     if (is_zero_bytes(key_bytes, 0, BLST_TS_SECRET_KEY_LENGTH))
     {
         sk->_is_zero_key = true;
     }
+
     return scope.Escape(wrapped);
 }
 
@@ -107,6 +130,8 @@ SecretKey::SecretKey(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
     Napi::HandleScope scope(env);
+    // Check that constructor was called from C++ and not JS. Externals can only
+    // be created natively.
     if (!info[0].IsExternal())
     {
         Napi::Error::New(env, "SecretKey constructor is private").ThrowAsJavaScriptException();
@@ -118,8 +143,10 @@ Napi::Value SecretKey::Serialize(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
     Napi::EscapableHandleScope scope(env);
+
     Napi::Buffer<uint8_t> serialized = Napi::Buffer<uint8_t>::New(env, BLST_TS_SECRET_KEY_LENGTH);
     _key->to_bendian(serialized.Data());
+
     return scope.Escape(serialized);
 }
 
@@ -127,11 +154,18 @@ Napi::Value SecretKey::ToPublicKey(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
     Napi::EscapableHandleScope scope(env);
+
+    // Pass void External to constructor so can tell if constructor was called
+    // from C or JS to prevent direct calls from JS to ensure proper setup
     Napi::Object wrapped = _module->_public_key_ctr.New({Napi::External<void *>::New(Env(), nullptr)});
+    // Setup object correctly.  Start with type tagging wrapper class.
     wrapped.TypeTag(&_module->_public_key_tag);
+    // Unwrap object to get native instance
     PublicKey *pk = PublicKey::Unwrap(wrapped);
+    // Derive public key from secret key. Default to jacobian coordinates
     pk->_jacobian.reset(new blst::P1{*_key});
     pk->_has_jacobian = true;
+
     return scope.Escape(wrapped);
 }
 
@@ -139,11 +173,15 @@ Napi::Value SecretKey::Sign(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
     Napi::EscapableHandleScope scope(env);
+
+    // Check for zero key and throw error to meet spec requirements
     if (_is_zero_key)
     {
         Napi::TypeError::New(env, "cannot sign message with zero private key").ThrowAsJavaScriptException();
         return scope.Escape(info.Env().Undefined());
     }
+
+    // Pull msg Value from info and check type is valid
     Napi::Value msg_value = info[0];
     if (!msg_value.IsTypedArray())
     {
@@ -156,13 +194,22 @@ Napi::Value SecretKey::Sign(const Napi::CallbackInfo &info)
         Napi::TypeError::New(env, "msg must be a BlstBuffer").ThrowAsJavaScriptException();
         return scope.Escape(info.Env().Undefined());
     }
+    // Convert to final Uint8Array type
     Napi::Uint8Array msg = msg_array.As<Napi::TypedArrayOf<uint8_t>>();
+
+    // Pass void External to constructor so can tell if constructor was called
+    // from C or JS to prevent direct calls from JS to ensure proper setup
     Napi::Object wrapped = _module->_signature_ctr.New({Napi::External<void *>::New(Env(), nullptr)});
+    // Setup object correctly.  Start with type tagging wrapper class.
     wrapped.TypeTag(&_module->_signature_tag);
+    // Unwrap object to get native instance
     Signature *sig = Signature::Unwrap(wrapped);
+    // Default to jacobian coordinates
     sig->_jacobian.reset(new blst::P2);
     sig->_has_jacobian = true;
+    // Hash to point and sign message
     sig->_jacobian->hash_to(msg.Data(), msg.ByteLength(), _module->_dst);
     sig->_jacobian->sign_with(*_key);
+    
     return scope.Escape(wrapped);
 }
