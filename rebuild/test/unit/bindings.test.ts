4d7e5fed105f001bc2e3a9067090ac457dbda35d
matthewkeil <me@matthewkeil.com>
2023-06-21 20:54:50 -0700

    test: expected exports

--- rebuild/test/unit/bindings.test.ts
+++ rebuild/test/unit/bindings.test.ts
@@ -2,6 +2,49 @@ import {expect} from "chai";
 import * as bindings from "../../lib";
 
 describe("bindings", () => {
+  describe("exports", () => {
+    const exports = new Set(Object.keys(bindings));
+    exports.delete("path");
+    exports.delete("default");
+
+    const expectedFunctions = [
+      "aggregatePublicKeys",
+      "aggregateSignatures",
+      "verify",
+      "fastAggregateVerify",
+      "aggregateVerify",
+      "verifyMultipleAggregateSignatures",
+    ];
+    const expectedClasses = ["PublicKey", "SecretKey", "Signature"];
+    const expectedConstants = ["CoordType", "BLST_CONSTANTS"];
+    after(() => {
+      expect(exports.size).to.equal(0);
+    });
+    it("should export all the expected functions", () => {
+      for (const expected of expectedFunctions) {
+        if (!exports.has(expected)) {
+          throw new Error(`Missing export: ${expected}`);
+        }
+        exports.delete(expected);
+      }
+    });
+    it("should export all the expected classes", () => {
+      for (const expected of expectedClasses) {
+        if (!exports.has(expected)) {
+          throw new Error(`Missing export: ${expected}`);
+        }
+        exports.delete(expected);
+      }
+    });
+    it("should export all the expected constants", () => {
+      for (const expected of expectedConstants) {
+        if (!exports.has(expected)) {
+          throw new Error(`Missing export: ${expected}`);
+        }
+        exports.delete(expected);
+      }
+    });
+  });
   describe("constants", () => {
     const {
       DST,
