8de932021a26983f8abd703f4e08b71be109e6a5
matthewkeil <me@matthewkeil.com>
2023-07-10 05:43:47 -0400

    test: add invalid key case for `badPublicKey`

--- rebuild/test/unit/PublicKey.test.ts
+++ rebuild/test/unit/PublicKey.test.ts
@@ -1,7 +1,7 @@
 import {expect} from "chai";
 import {BLST_CONSTANTS, CoordType, PublicKey, SecretKey} from "../../lib";
 import {expectEqualHex, expectNotEqualHex, sullyUint8Array} from "../utils";
-import {validPublicKey, SECRET_KEY_BYTES, invalidInputs} from "../__fixtures__";
+import {validPublicKey, SECRET_KEY_BYTES, invalidInputs, badPublicKey} from "../__fixtures__";
 
 describe("PublicKey", () => {
   it("should exist", () => {
@@ -53,6 +53,7 @@ describe("PublicKey", () => {
       });
       it("should throw on invalid key", () => {
         expect(() => PublicKey.deserialize(sullyUint8Array(validPublicKey.compressed))).to.throw("BLST_BAD_ENCODING");
+        expect(() => PublicKey.deserialize(badPublicKey)).to.throw("BLST_BAD_ENCODING");
       });
     });
   });
