346f91f161410a4f3db1aa57f461330c7237b4af
Matthew Keil <me@matthewkeil.com>
2024-01-05 15:38:16 -0500

    test: add fuzz tests

---
+++ rebuild/test/fuzz/fuzz.test.ts
@@ -0,0 +1,74 @@
+import fs from "fs";
+import {resolve} from "path";
+import {PromiseWithChild} from "child_process";
+import {testCases} from "./testCases";
+import {cmdStringExec} from "../../tools/exec";
+
+// timeout for all fuzz tests
+const TEST_TIMEOUT_IN_MINUTES = 20;
+const ROOT_DIR = resolve(__dirname, "..", "..");
+
+const timeout = setTimeout(exit, TEST_TIMEOUT_IN_MINUTES * 60 * 1000);
+// catch uncaught exceptions, then exit gracefully to generate coverage reports
+process.on("exit", exit);
+// catch ctrl+c and exit gracefully to generate coverage reports
+process.on("SIGINT", exit);
+
+const testingProcesses: PromiseWithChild<string>[] = [];
+async function exit(): Promise<void> {
+  for (const testProcess of testingProcesses) {
+    if (testProcess.child) {
+      testProcess.child.kill("SIGINT");
+    }
+  }
+  process.removeAllListeners("exit");
+  process.removeAllListeners("SIGINT");
+  clearTimeout(timeout);
+}
+
+function makeDirs(...paths: string[]): void {
+  for (const path of paths) {
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path, {recursive: true});
+    }
+  }
+}
+
+for (const testCase of testCases) {
+  const corpusDir = resolve(ROOT_DIR, "fuzz-tests", "corpus", testCase.name);
+  const coverageDir = resolve(ROOT_DIR, "fuzz-tests", "coverage", testCase.name);
+  makeDirs(corpusDir, coverageDir);
+
+  const cmd = [
+    `FUZZ_TEST_CASE=${testCase.name}`,
+    "node_modules/.bin/jazzer",
+    "dist/fuzz/test/fuzz/fuzzTarget",
+    corpusDir,
+    "--includes build",
+    "--includes deps",
+    "--includes src",
+    "--includes dist/lib",
+    "--includes dist/test",
+    "--excludes node_modules",
+    "--mode fuzzing",
+    "--timeout 1000", // individual test timeout. each run is max 1 second
+    "--sync false",
+    "--coverage true",
+    `--coverage_directory ${coverageDir}`,
+    "--coverage_reporters lcov",
+  ].join(" ");
+
+  console.log(`Running fuzz test: ${testCase.name}`);
+  testingProcesses.push(cmdStringExec(cmd, false, {cwd: ROOT_DIR}));
+}
+
+Promise.allSettled(testingProcesses)
+  .then((results) => {
+    console.log("All fuzz tests completed");
+    for (const result of results) {
+      if (result.status === "rejected") {
+        console.error(result.reason);
+      }
+    }
+  })
+  .finally(() => process.exit());
---
+++ rebuild/test/fuzz/testCases.ts
@@ -0,0 +1,56 @@
+import {SecretKey, PublicKey, Signature} from "../../lib";
+
+export interface FuzzTestCase {
+  name: string;
+  target: (data: Buffer) => any;
+  expectedErrors: string[];
+}
+
+export const testCases: FuzzTestCase[] = [
+  {
+    name: "SecretKey.fromKeygen",
+    target: (data: Buffer) => {
+      return SecretKey.fromKeygen(data);
+    },
+    expectedErrors: ["ikm must be greater than or equal to 32 bytes"],
+  },
+  {
+    name: "SecretKey.deserialize",
+    target: (data: Buffer) => {
+      return SecretKey.deserialize(data);
+    },
+    expectedErrors: ["BLST_ERROR: skBytes must be 32 bytes long"],
+  },
+  {
+    name: "secretKey.sign",
+    target: (data: Buffer) => {
+      return SecretKey.fromKeygen(Buffer.alloc(32), "*").sign(data);
+    },
+    expectedErrors: [],
+  },
+  {
+    name: "PublicKey.deserialize",
+    target: (data: Buffer) => {
+      return PublicKey.deserialize(data);
+    },
+    expectedErrors: [
+      "BLST_ERROR::BLST_PK_IS_INFINITY",
+      "BLST_ERROR::BLST_POINT_NOT_IN_GROUP",
+      "BLST_ERROR::BLST_POINT_NOT_ON_CURVE",
+      "BLST_ERROR::BLST_BAD_ENCODING",
+      "BLST_ERROR: pkBytes must be 48 or 96 bytes long",
+    ],
+  },
+  {
+    name: "Signature.deserialize",
+    target: (data: Buffer) => {
+      return Signature.deserialize(data);
+    },
+    expectedErrors: [
+      "BLST_ERROR::BLST_POINT_NOT_IN_GROUP",
+      "BLST_ERROR::BLST_POINT_NOT_ON_CURVE",
+      "BLST_ERROR::BLST_BAD_ENCODING",
+      "BLST_ERROR: sigBytes must be 96 or 192 bytes long",
+    ],
+  },
+];
