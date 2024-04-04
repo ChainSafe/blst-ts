0228c45ce3ee91e18f4dccd9c50785d461c70b13
matthewkeil <me@matthewkeil.com>
2024-04-04 14:56:11 +0700

    feat: remove swig unit tests

--- test/unit/lib/threads/runnable.js
+++
@@ -1,9 +0,0 @@
-const bls = require("../../../../dist/lib");
-
-const msg = Buffer.from("sample-msg");
-
-const sk = bls.SecretKey.fromKeygen(Buffer.alloc(32, 1));
-const pk = sk.toPublicKey();
-const sig = sk.sign(msg);
-
-bls.verify(msg, pk, sig);
