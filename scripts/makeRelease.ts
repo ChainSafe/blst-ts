97840cc9dbc7e35a627931949bab7dffeea35aad
matthewkeil <me@matthewkeil.com>
2024-04-09 13:57:15 +0700

    fix: update makeRelease to make more terse and add comments

--- scripts/makeRelease.ts
+++ scripts/makeRelease.ts
@@ -1,18 +1,21 @@
-import {cpSync, mkdirSync, readdirSync, rmSync} from "fs";
 import {resolve} from "path";
+import {cpSync, readdirSync, rmSync} from "fs";
 import {BINDINGS_FILE} from "../utils";
 
-const PREBUILD_FOLDER = resolve(__dirname, "..", "prebuild");
 const RELEASE_FOLDER = resolve(__dirname, "..", "release");
+const PREBUILD_FOLDER = resolve(__dirname, "..", "prebuild");
 
-cpSync(PREBUILD_FOLDER, RELEASE_FOLDER, {recursive: true});
-rmSync(PREBUILD_FOLDER, {recursive: true, force: true});
-mkdirSync(PREBUILD_FOLDER);
-
-const prebuilds = readdirSync(RELEASE_FOLDER);
-
-for (const binding of prebuilds) {
-  if (binding.endsWith(`-${process.versions.modules}-${BINDINGS_FILE}`)) {
-    cpSync(resolve(RELEASE_FOLDER, binding), resolve(PREBUILD_FOLDER, binding));
+/**
+ * Reads through the files in the PREBUILD_FOLDER.  Copies all to the
+ * RELEASE_FOLDER and then deletes the ones that are not slated for publishing
+ * to npm.  Only the currently supported version of node that lodestar uses
+ * is included in the npm tarball.  The rest of the consumers will pull from
+ * ths github release or have to build locally.
+ */
+for (const binding of readdirSync(PREBUILD_FOLDER)) {
+  const prebuiltPath = resolve(PREBUILD_FOLDER, binding);
+  cpSync(prebuiltPath, resolve(RELEASE_FOLDER, binding));
+  if (!binding.endsWith(`-${process.versions.modules}-${BINDINGS_FILE}`)) {
+    rmSync(prebuiltPath, {force: true});
   }
 }
