5e383d9948505dddfedbf4231a726e8bb88db730
matthewkeil <me@matthewkeil.com>
2024-04-25 21:25:22 +0700

    test: separate unit test cases

--- test/unit/verify.test.ts
+++ test/unit/verify.test.ts
@@ -19,10 +19,16 @@ describe("Verify", () => {
     it("should return a boolean", () => {
       expect(verify(testSet.message, testSet.publicKey, testSet.signature)).to.be.a("boolean");
     });
-    it("should default to false", () => {
-      expect(verify(sullyUint8Array(testSet.message), testSet.publicKey, testSet.signature)).to.be.false;
-      expect(verify(testSet.message, sullyUint8Array(testSet.publicKey.serialize()), testSet.signature)).to.be.false;
-      expect(verify(testSet.message, testSet.publicKey, sullyUint8Array(testSet.signature.serialize()))).to.be.false;
+    describe("should default to false", () => {
+      it("should handle invalid message", () => {
+        expect(verify(sullyUint8Array(testSet.message), testSet.publicKey, testSet.signature)).to.be.false;
+      });
+      it("should handle invalid publicKey", () => {
+        expect(verify(testSet.message, sullyUint8Array(testSet.publicKey.serialize()), testSet.signature)).to.be.false;
+      });
+      it("should handle invalid signature", () => {
+        expect(verify(testSet.message, testSet.publicKey, sullyUint8Array(testSet.signature.serialize()))).to.be.false;
+      });
     });
     it("should return true for valid sets", () => {
       expect(verify(testSet.message, testSet.publicKey, testSet.signature)).to.be.true;
@@ -35,12 +41,18 @@ describe("Verify", () => {
       const res = await resPromise;
       expect(res).to.be.a("boolean");
     });
-    it("should default to Promise<false>", async () => {
-      expect(await asyncVerify(sullyUint8Array(testSet.message), testSet.publicKey, testSet.signature)).to.be.false;
-      expect(await asyncVerify(testSet.message, sullyUint8Array(testSet.publicKey.serialize()), testSet.signature)).to
-        .be.false;
-      expect(await asyncVerify(testSet.message, testSet.publicKey, sullyUint8Array(testSet.signature.serialize()))).to
-        .be.false;
+    describe("should default to Promise<false>", () => {
+      it("should handle invalid message", async () => {
+        expect(await asyncVerify(sullyUint8Array(testSet.message), testSet.publicKey, testSet.signature)).to.be.false;
+      });
+      it("should handle invalid publicKey", async () => {
+        expect(await asyncVerify(testSet.message, sullyUint8Array(testSet.publicKey.serialize()), testSet.signature)).to
+          .be.false;
+      });
+      it("should handle invalid signature", async () => {
+        expect(await asyncVerify(testSet.message, testSet.publicKey, sullyUint8Array(testSet.signature.serialize()))).to
+          .be.false;
+      });
     });
     it("should return true for valid sets", async () => {
       expect(await asyncVerify(testSet.message, testSet.publicKey, testSet.signature)).to.be.true;
@@ -57,12 +69,18 @@ describe("Aggregate Verify", () => {
     it("should return a boolean", () => {
       expect(aggregateVerify([testSet.message], [testSet.publicKey], testSet.signature)).to.be.a("boolean");
     });
-    it("should default to false", () => {
-      expect(aggregateVerify([sullyUint8Array(testSet.message)], [testSet.publicKey], testSet.signature)).to.be.false;
-      expect(aggregateVerify([testSet.message], [sullyUint8Array(testSet.publicKey.serialize())], testSet.signature)).to
-        .be.false;
-      expect(aggregateVerify([testSet.message], [testSet.publicKey], sullyUint8Array(testSet.signature.serialize()))).to
-        .be.false;
+    describe("should default to false", () => {
+      it("should handle invalid message", () => {
+        expect(aggregateVerify([sullyUint8Array(testSet.message)], [testSet.publicKey], testSet.signature)).to.be.false;
+      });
+      it("should handle invalid publicKey", () => {
+        expect(aggregateVerify([testSet.message], [sullyUint8Array(testSet.publicKey.serialize())], testSet.signature))
+          .to.be.false;
+      });
+      it("should handle invalid signature", () => {
+        expect(aggregateVerify([testSet.message], [testSet.publicKey], sullyUint8Array(testSet.signature.serialize())))
+          .to.be.false;
+      });
     });
     it("should return true for valid sets", () => {
       expect(aggregateVerify([testSet.message], [testSet.publicKey], testSet.signature)).to.be.true;
@@ -75,23 +93,29 @@ describe("Aggregate Verify", () => {
       const res = await resPromise;
       expect(res).to.be.a("boolean");
     });
-    it("should default to Promise<false>", async () => {
-      expect(await asyncAggregateVerify([sullyUint8Array(testSet.message)], [testSet.publicKey], testSet.signature)).to
-        .be.false;
-      expect(
-        await asyncAggregateVerify(
-          [testSet.message],
-          [sullyUint8Array(testSet.publicKey.serialize())],
-          testSet.signature
-        )
-      ).to.be.false;
-      expect(
-        await asyncAggregateVerify(
-          [testSet.message],
-          [testSet.publicKey],
-          sullyUint8Array(testSet.signature.serialize())
-        )
-      ).to.be.false;
+    describe("should default to Promise<false>", () => {
+      it("should handle invalid message", async () => {
+        expect(await asyncAggregateVerify([sullyUint8Array(testSet.message)], [testSet.publicKey], testSet.signature))
+          .to.be.false;
+      });
+      it("should handle invalid publicKey", async () => {
+        expect(
+          await asyncAggregateVerify(
+            [testSet.message],
+            [sullyUint8Array(testSet.publicKey.serialize())],
+            testSet.signature
+          )
+        ).to.be.false;
+      });
+      it("should handle invalid signature", async () => {
+        expect(
+          await asyncAggregateVerify(
+            [testSet.message],
+            [testSet.publicKey],
+            sullyUint8Array(testSet.signature.serialize())
+          )
+        ).to.be.false;
+      });
     });
     it("should return true for valid sets", async () => {
       expect(await asyncAggregateVerify([testSet.message], [testSet.publicKey], testSet.signature)).to.be.true;
@@ -108,12 +132,21 @@ describe("Fast Aggregate Verify", () => {
     it("should return a boolean", () => {
       expect(fastAggregateVerify(testSet.message, [testSet.publicKey], testSet.signature)).to.be.a("boolean");
     });
-    it("should default to false", () => {
-      expect(fastAggregateVerify(sullyUint8Array(testSet.message), [testSet.publicKey], testSet.signature)).to.be.false;
-      expect(fastAggregateVerify(testSet.message, [sullyUint8Array(testSet.publicKey.serialize())], testSet.signature))
-        .to.be.false;
-      expect(fastAggregateVerify(testSet.message, [testSet.publicKey], sullyUint8Array(testSet.signature.serialize())))
-        .to.be.false;
+    describe("should default to false", () => {
+      it("should handle invalid message", () => {
+        expect(fastAggregateVerify(sullyUint8Array(testSet.message), [testSet.publicKey], testSet.signature)).to.be
+          .false;
+      });
+      it("should handle invalid publicKey", () => {
+        expect(
+          fastAggregateVerify(testSet.message, [sullyUint8Array(testSet.publicKey.serialize())], testSet.signature)
+        ).to.be.false;
+      });
+      it("should handle invalid signature", () => {
+        expect(
+          fastAggregateVerify(testSet.message, [testSet.publicKey], sullyUint8Array(testSet.signature.serialize()))
+        ).to.be.false;
+      });
     });
     it("should return true for valid sets", () => {
       expect(fastAggregateVerify(testSet.message, [testSet.publicKey], testSet.signature)).to.be.true;
@@ -126,23 +159,29 @@ describe("Fast Aggregate Verify", () => {
       const res = await resPromise;
       expect(res).to.be.a("boolean");
     });
-    it("should default to Promise<false>", async () => {
-      expect(await asyncFastAggregateVerify(sullyUint8Array(testSet.message), [testSet.publicKey], testSet.signature))
-        .to.be.false;
-      expect(
-        await asyncFastAggregateVerify(
-          testSet.message,
-          [sullyUint8Array(testSet.publicKey.serialize())],
-          testSet.signature
-        )
-      ).to.be.false;
-      expect(
-        await asyncFastAggregateVerify(
-          testSet.message,
-          [testSet.publicKey],
-          sullyUint8Array(testSet.signature.serialize())
-        )
-      ).to.be.false;
+    describe("should default to Promise<false>", () => {
+      it("should handle invalid message", async () => {
+        expect(await asyncFastAggregateVerify(sullyUint8Array(testSet.message), [testSet.publicKey], testSet.signature))
+          .to.be.false;
+      });
+      it("should handle invalid publicKey", async () => {
+        expect(
+          await asyncFastAggregateVerify(
+            testSet.message,
+            [sullyUint8Array(testSet.publicKey.serialize())],
+            testSet.signature
+          )
+        ).to.be.false;
+      });
+      it("should handle invalid signature", async () => {
+        expect(
+          await asyncFastAggregateVerify(
+            testSet.message,
+            [testSet.publicKey],
+            sullyUint8Array(testSet.signature.serialize())
+          )
+        ).to.be.false;
+      });
     });
     it("should return true for valid sets", async () => {
       expect(await asyncFastAggregateVerify(testSet.message, [testSet.publicKey], testSet.signature)).to.be.true;
